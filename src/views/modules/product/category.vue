ed5869921321f89e7a7c7cdfe20717a8ef5afd14
explore11 <prefect_start@163.com>
2021-01-03 00:06:00 +0800

    树形菜单的增加删除 页面显示

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
-    <el-tree
-      :data="menus"
-      :props="defaultProps"
-      @node-click="handleNodeClick"
-    ></el-tree>
+    <el-tree :data="menus" :props="defaultProps" @node-click="handleNodeClick" show-checkbox :expand-on-click-node="false" node-key="catId">
+      <span class="custom-tree-node" slot-scope="{ node, data }">
+        <span>{{ node.label }}</span>
+        <span>
+          <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+            新增
+          </el-button>
+          <el-button v-if="node.childNodes.length==0"  type="text" size="mini" @click="() => remove(node, data)">
+            删除
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
   </div>
 </template>
 
@@ -30,9 +38,18 @@ export default {
       this.$http({
         url: this.$http.adornUrl("/product/category/list/tree"),
         method: "get",
-      }).then(({data}) => {  // {data}  结构函数  返回的数据中  取data字段
-          this.menus = data.categoryEntityList;
+      }).then(({ data }) => {
+        // {data}  结构函数  返回的数据中  取data字段
+        this.menus = data.categoryEntityList;
       });
+    },
+    append(data) {
+      console.log("append", data);
+    },
+
+    remove(node, data) {
+      console.log("remove data", data);
+      console.log("remove node", node);
     },
   },
 
