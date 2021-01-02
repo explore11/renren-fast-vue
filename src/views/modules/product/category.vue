0f18ff46fb4e7193d4d079b9ee4f1abac9b7538e
explore11 <prefect_start@163.com>
2021-01-02 22:42:24 +0800

    dev

---
+++ src/views/modules/product/category.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <el-tree
+      :data="data"
+      :props="defaultProps"
+      @node-click="handleNodeClick"
+    ></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "category",
+  components: {},
+  data() {
+    return {
+      data: [],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    getDataListTree() {
+      this.dataListLoading = true;
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get",
+      }).then((data) => {
+        console.log("返回数据", data);
+      });
+    },
+  },
+
+  //计算属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    //调用接口
+    this.getDataListTree();
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
