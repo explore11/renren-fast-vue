3ba1ddb8839f2ebf0387e1703d5be94d3f1bf8ee
explore11 <prefect_start@163.com>
2021-01-31 20:46:23 +0800

    属性分组的基础功能

---
+++ src/views/modules/common/category.vue
@@ -0,0 +1,71 @@
+<!--  -->
+<template>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    node-key="catId"
+    ref="Menutree"
+    @node-click="nodeClick"
+  >
+  </el-tree>
+</template>
+
+<script>
+//这里可以导入其他文件（比如：组件，工具js，第三方插件js，json文件，图片文件等等）
+//例如：import 《组件名称》 from '《组件路径》';
+
+export default {
+  //import引入的组件需要注入到对象中才能使用
+  components: {},
+  data() {
+    //这里存放数据
+    return {
+      menus: [],
+      expandKeys: [],
+      defaultProps: {
+        children: "children",
+        label: "name"
+      }
+    };
+  },
+  //监听属性 类似于data概念
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  //方法集合
+  methods: {
+    // 点击事件
+    nodeClick(data, node, component) {
+      console.log("子组件点击事件。。。。", data, node, component);
+      this.$emit("category-node-click", data, node, component);
+    },
+    // 获取分类
+    getDataListTree() {
+      this.dataListLoading = true;
+      this.$http({
+        url: this.$http.adornUrl("/product/category/list/tree"),
+        method: "get"
+      }).then(({ data }) => {
+        // {data}  结构函数  返回的数据中  取data字段
+        this.menus = data.categoryEntityList;
+      });
+    }
+  },
+  //生命周期 - 创建完成（可以访问当前this实例）
+  created() {
+    //调用接口
+    this.getDataListTree();
+  },
+  //生命周期 - 挂载完成（可以访问DOM元素）
+  mounted() {},
+  beforeCreate() {}, //生命周期 - 创建之前
+  beforeMount() {}, //生命周期 - 挂载之前
+  beforeUpdate() {}, //生命周期 - 更新之前
+  updated() {}, //生命周期 - 更新之后
+  beforeDestroy() {}, //生命周期 - 销毁之前
+  destroyed() {}, //生命周期 - 销毁完成
+  activated() {} //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style scoped>
+</style>
